a70075c9d685af2597fa22d835310b8c54005b1a
Rafay <abdurrafay073@gmail.com>
2020-07-27 16:51:01 -0700

    started using passport modules

--- authDemo/app.js
+++ authDemo/app.js
@@ -1,14 +1,32 @@
-const express  = require('express'),
-      mongoose = require('mongoose'),
-      app = express()
+const express               = require('express'),
+      mongoose              = require('mongoose'),
+      passport              = require('passport'),
+      bodyParser            = require('body-parser'),
+      localStrategy         = require('passport-local'),
+      passportLocalMongoose = require('passport-local-mongoose'),
+      app                   = express(),
+      User                  = require('./models/user')
 
+// Config
 mongoose.connect('mongodb://localhost:27017/auth_demoapp', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).catch(err => {console.log(`DB error: ${err}`)})
+
+app.use(require('express-session')({
+  secre: 'the tea nepal is very hot',
+  resave: false,
+  saveUninitialized: false
+}))
+
 app.set('view engine', 'ejs')
+app.use(passport.initialize())
+app.use(passport.session())
 
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
+//ROUTES
 app.get('/', (req, res)=>{
   res.render('home')
 })
